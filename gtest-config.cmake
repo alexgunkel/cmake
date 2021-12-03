7475e920262775d4c896441662b5a1c207f24bbf
Alexander Gunkel <alexander.gunkel@check24.de>
2021-12-03 20:37:04 +0100

    do not install google test

--- gtest-config.cmake
+++ gtest-config.cmake
@@ -1,4 +1,5 @@
 include(FetchContent)
+option(INSTALL_GTEST OFF)
 FetchContent_Declare(
         googletest
         URL https://github.com/google/googletest/archive/609281088cfefc76f9d0ce82e1ff6c30cc3591e5.zip
