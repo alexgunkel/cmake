a219e6769dbc2e13e934c454862f0f5ec5afd522
Alexander Gunkel <alexander.gunkel@check24.de>
2021-12-03 16:58:33 +0100

    add cli11

--- gtest-config.cmake
+++ gtest-config.cmake
@@ -1,11 +1,7 @@
-include(ExternalProject)
-set(EXTERNAL_INSTALL_LOCATION ${PROJECT_SOURCE_DIR}/external)
+include(FetchContent)
+FetchContent_Declare(
+        googletest
+        URL https://github.com/google/googletest/archive/609281088cfefc76f9d0ce82e1ff6c30cc3591e5.zip
+)
 
-ExternalProject_Add(googletest_ep
-        GIT_REPOSITORY https://github.com/google/googletest
-        GIT_TAG 78fdd6c00b8fa5dd67066fbb796affc87ba0e075
-        GIT_PROGRESS true
-        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNAL_INSTALL_LOCATION} -DCMAKE_INSTALL_LIBDIR=lib64 -DCMAKE_CXX_FLAGS="-w"
-        )
-
-link_directories(${EXTERNAL_INSTALL_LOCATION}/lib64)
+FetchContent_MakeAvailable(googletest)
